471374fb90a1505892b5aa35bb0ae7f0d39c26cc
Micha Riedlinger <micha.riedlinger@active-group.de>
2025-12-18 16:35:22 +0100

    remove upper dependency bound and add nix overlay

--- flake.nix
+++ flake.nix
@@ -10,18 +10,16 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, webview }:
-    flake-utils.lib.eachSystem [
-      "aarch64-darwin"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "x86_64-linux"
-    ] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        syncWebviewCore = pkgs.writeShellApplication {
+  outputs = inputs@{ self, nixpkgs, flake-utils, webview }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      mkSyncWebviewCore = pkgs:
+        pkgs.writeShellApplication {
           name = "sync-webview-core";
           text = ''
             set -euo pipefail
@@ -36,26 +34,41 @@
             chmod -R u+w "''${dest}"
           '';
         };
-        hsWebviewSrc = pkgs.runCommand "hs-webview-src" { } ''
-          cp -r ${./.} $out
-          chmod -R u+w $out
-          cd $out
-          ${syncWebviewCore}/bin/sync-webview-core cbits
-        '';
-        platformLibs =
-          if pkgs.stdenv.isDarwin then
-            [ pkgs.apple-sdk ]
-          else
-            (with pkgs; [ gtk3 webkitgtk_4_1 ]);
-        haskellPackages = pkgs.haskellPackages.override {
-          overrides = final: prev: {
+      platformLibsFor = pkgs:
+        if pkgs.stdenv.isDarwin then
+          [ pkgs.apple-sdk ]
+        else
+          (with pkgs; [ gtk3 webkitgtk_4_1 ]);
+      hsWebviewOverlay = final: prev:
+        let
+          syncWebviewCore = mkSyncWebviewCore final;
+          hsWebviewSrc = final.runCommand "hs-webview-src" { } ''
+            cp -r ${./.} $out
+            chmod -R u+w $out
+            cd $out
+            ${syncWebviewCore}/bin/sync-webview-core cbits
+          '';
+          platformLibs = platformLibsFor final;
+        in
+        {
+          haskellPackages = prev.haskellPackages.extend (hself: hsuper: {
             hs-webview =
-              (prev.callCabal2nix "hs-webview" hsWebviewSrc { }).overrideAttrs (old: {
+              (hself.callCabal2nix "hs-webview" hsWebviewSrc { }).overrideAttrs (old: {
                 buildInputs = (old.buildInputs or [ ]) ++ platformLibs;
               });
-          };
+          });
         };
-        hsWebview = haskellPackages.hs-webview;
+      overlays = [ hsWebviewOverlay ];
+    in
+    flake-utils.lib.eachSystem systems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = overlays;
+        };
+        syncWebviewCore = mkSyncWebviewCore pkgs;
+        platformLibs = platformLibsFor pkgs;
+        hsWebview = pkgs.haskellPackages.hs-webview;
       in
       {
         packages.default = hsWebview;
@@ -65,11 +78,11 @@
           program = "${syncWebviewCore}/bin/sync-webview-core";
         };
 
-        devShells.default = haskellPackages.shellFor {
+        devShells.default = pkgs.haskellPackages.shellFor {
           packages = p: [ p.hs-webview ];
           buildInputs = platformLibs
             ++ [ pkgs.cabal-install pkgs.pkg-config ]
-            ++ (with haskellPackages; [ haskell-language-server ghcid hlint hoogle fourmolu ]);
+            ++ (with pkgs.haskellPackages; [ haskell-language-server ghcid hlint hoogle fourmolu ]);
           nativeBuildInputs = [ pkgs.pkg-config syncWebviewCore ];
           shellHook = ''
             if [ ! -f cbits/webview.cc ]; then
@@ -78,5 +91,7 @@
             fi
           '';
         };
-      });
+      }) // {
+      overlays.default = nixpkgs.lib.composeManyExtensions overlays;
+    };
 }
